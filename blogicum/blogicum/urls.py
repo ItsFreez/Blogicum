6de5ea62c8581ba59fef7e55841e60c7e3f6968e
Игорь Митяшин <ItsFreez@yandex.ru>
2023-07-10 13:06:22 +0300

    Add registration and login for users

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+from django.urls import include, path, reverse_lazy
 
 
 handler404 = 'core.views.page_not_found'
@@ -8,5 +10,14 @@ handler500 = 'core.views.server_error'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls', namespace='blog')),
+    path('auth/', include('django.contrib.auth.urls')),
+    path('auth/registration',
+         CreateView.as_view(
+             template_name='registration/registration_form.html',
+             form_class=UserCreationForm,
+             success_url=reverse_lazy('blog:index'),
+         ),
+         name='registration',
+         ),
     path('pages/', include('pages.urls', namespace='pages')),
 ]
