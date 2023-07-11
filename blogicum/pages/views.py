5e580246c4a8955d6d9d42220aea3286b829d9f7
Игорь Митяшин <ItsFreez@yandex.ru>
2023-07-11 20:28:18 +0300

    Add docstrings for pages.views

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -3,20 +3,25 @@ from django.views.generic import TemplateView
 
 
 class AboutPage(TemplateView):
+    """Отобразить страницу о сайте."""
     template_name = 'pages/about.html'
 
 
 class RulesPage(TemplateView):
+    """Отобразить правила сайта."""
     template_name = 'pages/rules.html'
 
 
 def csrf_failure(request, reason=''):
+    """Кастомная ошибка 403."""
     return render(request, 'pages/403csrf.html', status=403)
 
 
 def page_not_found(request, exception):
+    """Кастомная ошибка 404."""
     return render(request, 'pages/404.html', status=404)
 
 
 def server_error(request):
+    """Кастомная ошибка 500."""
     return render(request, 'pages/500.html', status=500)
