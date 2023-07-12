1b19c6a4ff846b1de86e927b48e9f3ccb3810fb8
Игорь Митяшин <ItsFreez@yandex.ru>
2023-07-12 12:36:31 +0300

    Add post_urls list for url posts

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -1,49 +1,43 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 app_name = 'blog'
 
+post_urls = [
+    path('create/',
+         views.PostCreateView.as_view(),
+         name='create_post'),
+    path('<int:pk>/',
+         views.PostDetailView.as_view(),
+         name='post_detail'),
+    path('<int:pk>/edit/',
+         views.PostUpdateView.as_view(),
+         name='edit_post'),
+    path('<int:pk>/delete/',
+         views.PostDeleteView.as_view(),
+         name='delete_post'),
+    path('<int:pk>/comment/',
+         views.CommentCreateView.as_view(),
+         name='add_comment'),
+    path('<int:id>/edit_comment/<int:pk>/',
+         views.CommentUpdateView.as_view(),
+         name='edit_comment'),
+    path('<int:id>/delete_comment/<int:pk>/',
+         views.CommentDeleteView.as_view(),
+         name='delete_comment')
+]
+
 urlpatterns = [
     path('', views.IndexListView.as_view(), name='index'),
-    path('posts/<int:pk>/',
-         views.PostDetailView.as_view(),
-         name='post_detail'
-         ),
+    path('posts/', include(post_urls)),
     path('category/<slug:category_slug>/',
          views.CategoryListView.as_view(),
-         name='category_posts'
-         ),
+         name='category_posts'),
     path('profile/<slug:username>/',
          views.ProfileListView.as_view(),
-         name='profile'
-         ),
+         name='profile'),
     path('profile/<int:pk>/edit_profile/',
          views.ProfileUpdateView.as_view(),
-         name='edit_profile'
-         ),
-    path('posts/create/',
-         views.PostCreateView.as_view(),
-         name='create_post'
-         ),
-    path('posts/<int:pk>/edit/',
-         views.PostUpdateView.as_view(),
-         name='edit_post'
-         ),
-    path('posts/<int:pk>/delete/',
-         views.PostDeleteView.as_view(),
-         name='delete_post'
-         ),
-    path('posts/<int:pk>/comment/',
-         views.CommentCreateView.as_view(),
-         name='add_comment'
-         ),
-    path('posts/<int:id>/edit_comment/<int:pk>/',
-         views.CommentUpdateView.as_view(),
-         name='edit_comment'
-         ),
-    path('posts/<int:id>/delete_comment/<int:pk>/',
-         views.CommentDeleteView.as_view(),
-         name='delete_comment'
-         )
+         name='edit_profile'),
 ]
--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -19,7 +19,6 @@ urlpatterns = [
              form_class=UserCreationForm,
              success_url=reverse_lazy('blog:index'),
          ),
-         name='registration',
-         ),
+         name='registration'),
     path('pages/', include('pages.urls', namespace='pages')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
